829c5ed9c265c03af836f26befef66f84389e6d2
aditya-pethe <apethe100@DESKTOP-IMQOEF6.localdomain>
2020-04-22 17:54:14 -0500

    configure init

---
+++ configure.cpp
@@ -0,0 +1,9 @@
+// File: cachesimulator.cpp
+// Author(s): Joseph Carpman, Aditya Pethe
+// Date: 04/22/2020
+// Section: 510
+// E-mail: jcarpman100@tamu.edu, apeth100@tamu.edu
+
+#include <configure.h>
+
+//define all the configure funcitons here
